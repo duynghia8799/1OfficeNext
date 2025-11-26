518b598b78bfb6de491ac2db54a376caabc61268
doleduy59 <doleduy59@gmail.com>
2025-11-26 18:12:38 +0700

    full html ai-agents

--- ai_agents.php
+++ ai_agents.php
@@ -11,9 +11,13 @@ $page_data = file_exists($data_file_path) ? require $data_file_path : [];
 $tab1 = isset($page_data['tang_toc_data']) ? $page_data['tang_toc_data'] : [];
 $tab2 = isset($page_data['tich_hop_data']) ? $page_data['tich_hop_data'] : [];
 
-$svg_hero = file_get_contents(get_template_directory_uri() . '/assets/images/ai_agents_all.svg');
+$svg_hero = file_get_contents(get_template_directory_uri() . '/assets/images/ai_agents_hero.svg');
 if ($svg_hero === false) {
     $svg_hero = "<p style='color: red;'>Lỗi: Không tìm thấy file SVG.</p>";
+}
+$svg_hesinhthai = file_get_contents(get_template_directory_uri() . '/assets/images/ai_agents_hesinhthai.svg');
+if ($svg_hesinhthai === false) {
+    $svg_hesinhthai = "<p style='color: red;'>Lỗi: Không tìm thấy file SVG.</p>";
 }
 ?>
 
@@ -27,18 +31,24 @@ if ($svg_hero === false) {
                 </h1>'
                 <p>Làm việc thông minh hơn với trợ lý AI dành cho doanh nghiệp của bạn.
                     Đơn giản hoá mọi tác vụ từ truy xuất, phân tích dữ liệu đến ra quyết định</p>
-                <?= get_template_part('template_parts/callout_parts/dk_chat_ai', 'template') ?>
+                <?php get_template_part('template_parts/callout_parts/dk_chat_ai', 'template'); ?>
             </div>
             <div class="img-container">
-                <?php echo $svg_hero ?>
+                <?= $svg_hero ?>
             </div>
-
         </div>
     </div>
 </section>
 
-<section class>
-
+<section class="ai-agents-hesinhthai">
+    <?= $svg_hesinhthai ?>
+    <div class="container">
+        <div class="row">
+            <h1 class="title-section">Hệ sinh thái 1AI Agents</h1>
+            <p class="sub-title-section">Kết nối dữ liệu, dự đoán xu hướng và đề xuất hành động tối ưu</p>
+            <p class="content">Vận hành doanh nghiệp <span class="br-line">thông minh với AI Agents</span></p>
+        </div>
+    </div>
 </section>
 
 <section class="ai-agents-tangtoc">
@@ -51,8 +61,6 @@ if ($svg_hero === false) {
     </div>
 </section>
 
-
-
 <section class="ai-agents-tichhop">
     <div class="container">
         <div class="row tichhop-row">
@@ -64,9 +72,93 @@ if ($svg_hero === false) {
     </div>
 </section>
 
+<section class="ai-agents-chuyendoi">
+    <img class="ai-agents-chuyendoi-bg" src="<?= get_template_directory_uri() . '/assets/images/ai_agents_chuyendoi_bg.svg' ?>" alt="">
+    <div class="container">
+        <div class="row">
+            <h1 class="title-section">Chuyển đổi cách doanh nghiệp vận hành
+                <span class="br-line">bằng bộ công cụ AI mạnh mẽ</span></h1>
+            <div class="content">
+                <div class="item">
+                    <img src="<?= get_template_directory_uri() . '/assets/images/icons/ai_agents_chuyendoi_item1.svg' ?>" alt="">
+                    <h2>Khả năng tích hợp</h2>
+                    <p>Tích hợp với toàn bộ quy trình vận hành <span class="br-line">của doanh nghiệp thông qua nền tảng <span class="br-line">All-in-one</span></span></p>
+                </div>
+                <div class="item">
+                    <img src="<?= get_template_directory_uri() . '/assets/images/icons/ai_agents_chuyendoi_item2.svg' ?>" alt="">
+                    <h2>Phối hợp đồng bộ</h2>
+                    <p>Các AI Agents được tích hợp chung trên một nền tảng, phối hợp thực hiện các nhiệm vụ phức tạp một cách đồng bộ</p>
+                </div>
+            
+                <div class="item">
+                    <img src="<?= get_template_directory_uri() . '/assets/images/icons/ai_agents_chuyendoi_item3.svg' ?>" alt="">
+                    <h2>Hiểu ngữ cảnh sâu</h2>
+                    <p>Hiểu ngữ cảnh doanh nghiệp sâu nhờ khả năng tự động truy xuất và phân tích dữ liệu thông minh</p>
+                </div>
+            
+                <div class="item">
+                    <img src="<?= get_template_directory_uri() . '/assets/images/icons/ai_agents_chuyendoi_item4.svg' ?>" alt="">
+                    <h2>Tự động hoá</h2>
+                    <p>Không chỉ đưa ra gợi ý, 1AI Agents còn được giao quyền thay người dùng tự động xử lý tác vụ và quy trình</p>
+                </div>
 
+                <div class="item">
+                    <img src="<?= get_template_directory_uri() . '/assets/images/icons/ai_agents_chuyendoi_item5.svg' ?>" alt="">
+                    <h2>Bản địa hóa</h2>
+                    <p>AI Agents 1Office được huấn luyện từ dữ liệu hàng ngàn doanh nghiệp Việt, nên thấu hiểu đặc thù quản trị và hành vi người dùng nội địa</p>
+                </div>
 
+                <div class="item">
+                    <img src="<?= get_template_directory_uri() . '/assets/images/icons/ai_agents_chuyendoi_item6.svg' ?>" alt="">
+                    <h2>Bảo mật</h2>
+                    <p>Tuyệt đối bảo mật dữ liệu cá nhân và doanh nghiệp nhờ tích hợp sâu với quyền người dùng trên hệ thống</p>
+                </div>
+            </div>
+            
+                
+            
+        </div>
+    </div>
+</section>
 
-<div style="height: 200px; width:100%; background-color:grey;"></div>
+<section class="ai-agents-kientao">
+    <div class="container">
+        <div class="row">
+            <h1 class="title-section">Kiến tạo doanh nghiệp vận hành <span class="br-line">thông minh, tự động</span></h1>
+            <div class="content">
+                <div class="item">
+                    <h1>80<span>%</span></h1>
+                    <h2>Tiết kiệm chi phí</h2>
+                    <p>Nền tảng AI Agents giúp loại bỏ chi phí cho các công cụ, phần mềm rời rạc, tối ưu nguồn lực và nâng cao hiệu suất toàn doanh nghiệp</p>
+                </div>
+                <div class="line"></div>
+                <div class="item">
+                    <h1>24<span>h</span></h1>
+                    <h2>Tiết kiệm hàng tuần</h2>
+                    <p>Với sự kết hợp của AI và No-code, 1Office giúp người dùng tiết kiệm thời gian bằng việc tự động hoá công việc và giảm tải các tác vụ lặp lại</p>                
+                </div>
+                <div class="line"></div>
+                <div class="item">
+                    <h1>03<span>lần</span></h1>
+                    <h2>Xử lý nhanh hơn</h2>
+                    <p>Các tác vụ từ phê duyệt, truy xuất dữ liệu, báo cáo đến ra quyết định đều được xử lý tức thời nhờ hệ thống AI thông minh và khả năng tự động hoá liền mạch</p>
+                </div>
+            </div>
+        </div>
+    </div>  
+</section>
+<section class="ai-agents-buttoc">
+    <img class="ai-agents-buttoc-bg" src="<?= get_template_directory_uri() . '/assets/images/ai_agents_buttoc_bg.svg' ?>" alt="">
+    <div class="container">
+        <div class="row">
+            <div class="content">
+                <img class="ai-light-bg" src="<?= get_template_directory_uri() . '/assets/images/ai_light_bg.svg' ?>" alt="">
+                <h1 class="title-section">Bứt tốc trong kỷ nguyên AI. Khai thác tối đa <span class="br-line">sức mạnh của tự động hoá</span></h1>
+                <?php get_template_part('template_parts/callout_parts/dk_chat_ai', 'template') ?>
+            </div>
+        </div>
+    </div>  
+</section>
+
 
 <?php get_footer();
