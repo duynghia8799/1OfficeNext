991d23b1cc2b09e8ff69a257658ad408aabab613
doleduy59 <doleduy59@gmail.com>
2025-12-29 17:52:40 +0700

    social wip

---
+++ social.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Template Name: Social 1Office Next
+ */
+
+get_header(); ?>
+
+<div class="social-page">
+
+<section class="social-moitruong">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="social-expand-container">
+                    <div class="expand-item active bg1" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/social_item_1.svg');">
+                        <div class="content">
+                            <h2>Nhân sự</h2>
+                            <p>Mạng nội bộ trở thành trung tâm thông tin giúp nhân viên cập nhật <br/>tin tức, trao đổi dễ dàng và tiếp cận tài nguyên làm việc mọi lúc</p>
+                        </div>
+                    </div>
+                    <div class="expand-item bg2" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/social_item_2.svg');">
+                        <div class="content">
+                            <h2>Quản lý cấp trung</h2>
+                            <p>Hỗ trợ quản lý nắm bắt nhanh tình hình đội ngũ, theo dõi <br/>luồng thông tin và tương tác hai chiều với nhân viên</p>
+                        </div>
+                    </div>
+                    <div class="expand-item bg3" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/social_item_3.svg');">
+                        <div class="content">
+                            <h2>Lãnh đạo & CxO</h2>
+                            <p>Cung cấp góc nhìn toàn cảnh về tổ chức, văn hóa, mức độ gắn kết <br/>của nhân và truyền tải thông điệp quan trọng hiệu quả</p>
+                        </div>
+                    </div>
+                    <div class="expand-item bg4" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/social_item_4.svg');">
+                        <div class="content">
+                            <h2>Doanh nghiệp</h2>
+                            <p>Tạo ra không gian làm việc số chuyên nghiệp, tập trung mọi thông tin, <br/>tài nguyên, giúp vận hành hiệu quả và xây dựng văn hóa bền vững</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+</div>
+
+
+<?php get_footer(); ?>
