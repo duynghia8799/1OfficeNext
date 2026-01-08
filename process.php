748b9d0a7ebf895ddb264f42160a26aeaa05ef50
doleduy59 <doleduy59@gmail.com>
2026-01-08 07:45:22 +0700

    .

---
+++ process.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Template Name: Process 1Office Next
+ */
+
+get_header();
+?>
+<div class="process-page">
+
+    <section class="process-hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="title-section">Tự động hoá toàn diện quy trình nghiệp vụ</h1>
+                    <p class="sub-title-section">Xây dựng luồng xử lý linh hoạt, tăng tốc phê duyệt, minh bạch trách nhiệm và đảm bảo <br />mọi quy trình được thực thi liền mạch</p>
+                    <?php get_template_part('template-parts/callout_parts/dk_chat_ai', 'button'); ?>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+</div>
+<?php get_footer(); ?>
