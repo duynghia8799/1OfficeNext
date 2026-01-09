fb93b31346dd15638a2628e17cc4cd5316036266
doleduy59 <doleduy59@gmail.com>
2026-01-09 13:44:29 +0700

    animation process-hero

--- process.php
+++ process.php
@@ -15,7 +15,9 @@ get_header();
                     <h1 class="title-section">Tự động hoá toàn diện quy trình nghiệp vụ</h1>
                     <p class="sub-title-section">Xây dựng luồng xử lý linh hoạt, tăng tốc phê duyệt, minh bạch trách nhiệm và đảm bảo <br/>mọi quy trình được thực thi liền mạch</p>
                     <?php get_template_part('template-parts/callout_parts/dk_chat_ai', 'button'); ?>
-                    <?php echo file_get_contents(get_stylesheet_directory() . '/assets/images/for-animated/process_hero.svg'); ?>
+                    <div class="demo">
+                        <?php echo file_get_contents(get_stylesheet_directory() . '/assets/images/for-animated/process_hero.svg'); ?>
+                    </div>
                 </div>
             </div>
         </div>
