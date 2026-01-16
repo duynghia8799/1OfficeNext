b957c59991adb18ed4a15e1c10f755e13d4f2677
doleduy59 <doleduy59@gmail.com>
2026-01-16 17:13:34 +0700

    .

---
+++ tailieu.php
@@ -0,0 +1,153 @@
+<?php
+
+/**
+ * Template Name: Văn bản 1OfficeNext
+ */
+
+get_header(); ?>
+
+<div class="tailieu-page">
+
+<section class="tailieu-hero">
+    <div class="controller">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="title-section">Số hoá và quản lý tài liệu tập trung</h1>
+                <p class="sub-title-section">Lưu trữ và quản lý tập trung toàn bộ tài nguyên ngay trên một phần mềm, giúp <br/>người dùng tiết kiệm thời gian tìm kiếm và hạn chế tối đa tình trạng thất lạc tài liệu</p>
+                <?php get_template_part('template-parts/callout_parts/dk_chat_ai', 'button'); ?>  
+                <img src="" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="tailieu-hero-bottom">
+    <div class="controller">
+        <div class="row">
+            <div class="col-12">
+                <div class="content">
+                    <div class="item">
+                        <img src="" alt="">
+                        <h2>Hạn chế rủi ro thất lạc</h2>
+                        <p>Mọi tài liệu nội bộ được lưu trữ tập trung, <br/>phân quyền chặt chẽ giúp đảm bảo an <br/>toàn và kiểm soát truy cập hiệu quả</p>
+                    </div>
+                    <div class="item">
+                        <img src="" alt="">
+                        <h2>Cập nhật nhanh chóng</h2>
+                        <p>Cho phép chỉnh sửa, thảo luận và cập <br/>nhật tài liệu ngay trên hệ thống, giúp đội ngũ <br/>luôn làm việc với thông tin mới nhất</p>
+                    </div>
+                    <div class="item">
+                        <img src="" alt="">
+                        <h2>Kho tri thức tập trung</h2>
+                        <p>Dễ dàng tìm kiếm, truy cập & sử dụng tài <br/>liệu theo đúng phòng ban, chủ đề hoặc <br/>danh mục đã được tổ chức khoa học</p>
+                    </div>
+                    <div class="item">
+                        <img src="" alt="">
+                        <h2>Đảm bảo tính nhất quán</h2>
+                        <p>Các mẫu tài liệu, hướng dẫn và quy định <br/>được tập trung quản lý, giảm sai sót và <br/>giúp thống nhất quy trình làm việc</p>
+                    </div>
+                </div>
+                <div class="bottom">
+                    <div class="left">
+                        <img src="" alt="">
+                        <p>Đảm bảo tính đồng bộ và bảo mật</p>
+                    </div>
+                    <p>Chia sẻ, lưu trữ và sử dụng tài liệu một cách khoa học, minh bạch và đồng bộ trên cùng một hệ thống.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="tailieu-quanly">
+    <div class="controller">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="title-section">Quản lý và lưu trữ tài liệu thống nhất toàn doanh nghiệp</h1>
+                <?php get_template_part('template-parts/tabs/animation_tab', null, array(
+                    "name" => "tailieu-quanly",
+                    "tabs" => array(
+                        [
+                            
+                        ],
+                        
+                    )
+                )); ?>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="tailieu-tangtoc">
+    <div class="controller">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="title-section">Tăng tốc tra cứu, đối chiếu và phân tích tài liệu với AI</h1>
+                <div class="content">
+                    <div class="item">
+                        <h3>Truy xuất thông tin tức thì</h3>
+                        <p>Tự động quét toàn bộ tài liệu, quy trình và dữ <br/>liệu liên quan để trả lời chính xác các câu hỏi, <br/>giúp người dùng tìm thông tin mà không cần <br/>mở từng file</p>
+                    </div>
+                    <div class="item">
+                        <h3>Đối chiếu dữ liệu thông minh</h3>
+                        <p>So sánh nội dung tài liệu với dữ liệu thực tế <br/>hoặc chính sách áp dụng, nhanh chóng phát <br/>hiện sai lệch và đưa ra nhận định trung thực <br/>giúp người dùng quyết đinh chuẩn xác</p>
+                    </div>
+                    <div class="item">
+                        <h3>Tóm tắt tài liệu theo ngữ cảnh</h3>
+                        <p>Phân tích nội dung dài, trích xuất các điểm <br/>quan trọng và trình bày ngắn gọn theo đúng <br/>nhu cầu người dùng, hỗ trợ đọc - hiểu tài liệu <br/>chỉ trong vài giây</p>
+                    </div>
+                </div>
+                <img src="" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="controller">
+        <div class="row">
+            <div class="col">
+                <div class="content">
+                    <h1 class="title-section">Kho tài liệu trực tuyến <br/>dành cho mọi phòng ban</h1>
+                    <div class="item">
+                        <img src="" alt="">
+                        <p>Giao diện trực quan</p>
+                    </div>
+                    <div class="item">
+                        <img src="" alt="">
+                        <p>Sắp xếp và phân loại khoa học</p>
+                    </div>
+                    <div class="item">
+                        <img src="" alt="">
+                        <p>Truy xuất nhanh chóng</p>
+                    </div>
+                    <div class="item">
+                        <img src="" alt="">
+                        <p>Phân quyền chặt chẽ</p>
+                    </div>
+                    <div class="item">
+                        <img src="" alt="">
+                        <p>Theo dõi lịch sử thao tác</p>
+                    </div>
+                    <div class="item">
+                        <img src="" alt="">
+                        <p>Lưu trữ tài liệu đa nguồn</p>
+                    </div>
+                    <div class="buttons">
+                        <button><img src="" alt=""></button>
+                        <button><img src="" alt=""></button>
+                    </div>
+                </div>
+                <img class="phones" src="" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+</div>
+
+<?php get_footer(); ?>
